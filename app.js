2d870e6639cf884b2c1d9ef81ed59f88cd07126b
yousef-ghanim <yousefghanim85@gmail.com>
2020-11-09 11:13:34 +0000

    background

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const navBtn = document.querySelector(".nav-btn");
 const navLinks = document.querySelector("body");
+const nav = document.querySelector(".nav-links")
 
 navBtn.addEventListener("click", () => {
   navLinks.classList.toggle("change");
 });
+
+window.addEventListener("scroll", ()=>{
+   const navHeight = nav.getBoundingClientRect().height;
+const scrollHeight = window.pageYOffset;
+  if(scrollHeight > navHeight){
+    nav.classList.add("nav-fixed");
+  }else{
+        nav.classList.remove("nav-fixed");
+
+  }
+})
